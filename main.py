12dc0e628a69430501b343c98e35e47ae866952d
Mason Salcido <salcido.mts@gmail.com>
2022-02-14 22:39:02 -0700

    Changed storage from file creation to SQLite

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
 import asyncio
 from datetime import datetime
-import random
-import time
+import os
+import sqlite3
 import discord
 from discord.ext import commands
-import os
+from configparser import ConfigParser
 from dotenv import load_dotenv
 
 client = commands.Bot(command_prefix='!')
+cfg = ConfigParser()
+cfg.read('config.ini')
 load_dotenv()
+
+def create_connection(db_file):
+    conn = None
+    try:
+        print('[INFO] Attempting to connect to DB...')
+        conn = sqlite3.connect(db_file)
+        print('[SUCCESS] Connected to DB...')
+        return conn
+    except sqlite3.Error as error:
+        print('[ERROR] Error connecting to DB -', error)
+    
+    return conn
+
+def create_table(conn, create_table_sql):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(create_table_sql)
+    except sqlite3.Error as error:
+        print('[ERROR] Unable to create table -', error)
+
+def create_insert_sql(conn, name, quote):
+    try:
+        c = conn.cursor()
+        sql = f'INSERT INTO quotes(name, quote, create_date) VALUES (\'{name}\', \'{quote}\', DATE(\'now\'));'
+        c.execute(sql)
+        conn.commit()
+    except sqlite3.Error as error:
+        print('[ERROR] Unable to insert into table - ', error)
+
+database_name = cfg.get('database','Name')
+database = rf'{database_name}.db'
+sql_create_quotes_table = ''' CREATE TABLE IF NOT EXISTS quotes (
+    id INTEGER PRIMARY KEY,
+    name text NOT NULL,
+    quote text NOT NULL,
+    create_date text NOT NULL); '''
+
+
+
+conn = create_connection(database)
+
+if conn is not None:
+    create_table(conn, sql_create_quotes_table)
+else:
+    print('[ERROR] Unable to create database connection')
+
 
 @client.event
 async def on_ready():
-
-    dir = 'Quotes'
-    all_path = '{}/All'.format(dir)
-    
-    print('Checking for \'Quotes\' folder...')
-    create_dir(dir)
-
-    print('Attempting to create \'All\' folder...')
-    create_dir(all_path)
 
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='all your thoughts'))
 
@@ -28,13 +67,14 @@ async def on_ready():
 
 @client.command()
 async def add(ctx, user='None' ,*args):
-    timestr = time.strftime('%m%d%y %H%M%S')
 
     if len(args) < 1:
         await send_embed(ctx, ':x:\tUnable to process command\t:x:', '!add @user <quote>', discord.Color.red(), '', '')
+        print('[ERROR] Not enough arguments supplied')
         return
 
-    await send_embed(ctx, ':repeat:\tAttempting to create quote...\t:repeat:', '', discord.Color.green(), '', '')
+    print('[INFO] Attempting to create new quote...')
+    await send_embed(ctx, ':repeat:\tAttempting to create quote...\t:repeat:', '', discord.Color.teal(), '', '')
     await asyncio.sleep(3)
     
     removed_chars = '<@!>'
@@ -45,68 +85,59 @@ async def add(ctx, user='None' ,*args):
         user_object = await client.fetch_user(user)
     except (discord.errors.NotFound, discord.errors.HTTPException):
         await send_embed(ctx,':x:\tUnable to locate @{}\t:x:'.format(user), 'Must tag user', discord.Color.red(),'','')
+        print('[ERROR] Unable to located user')
         return
 
     user_str = str(user_object)
 
-    def create_quote():
-        dir = 'Quotes'
-        quoted_user = user_str
-        full_path = '{0}/{1}'.format(dir,quoted_user)
-        all_path = '{}/All'.format(dir)
 
-        print('Attempting to make directory: ' + full_path)
-        create_dir(full_path)
+    content = ' '.join(args)
+    create_insert_sql(conn, user_str, content)
 
-        file_name = '{}.txt'.format(timestr)
-        complete_file = os.path.join(full_path, file_name)
-        all_file = os.path.join(all_path, file_name)
-        print('Attempting to create quote file: ' + file_name)
-        try:
-            f = open(complete_file, 'x')
-            all_f = open(all_file, 'x')
-            print('[SUCCESS] Created quote:', file_name)
-        except FileExistsError:
-            print('[FAIL] Unable to create quote file...try again')
-
-        content = ' '.join(args)
-        f.write('{0}\n"{1}"'.format(quoted_user, content))
-        all_f.write('{0}\n"{1}"'.format(quoted_user, content))
-
-    create_quote()
     await send_embed(ctx, ':white_check_mark:\tSuccessfully created quote\t:white_check_mark:', '"{}"'.format(' '.join(args)), discord.Color.green(),'', '')
+    print(f'[SUCCESS] Inserted new quote to DB - @{user_str}: \'{content}\'')
 
 @client.command()
 async def quote(ctx):
-    all_path = 'Quotes/All'
 
-    if len(os.listdir(all_path)) == 0:
+    print('[INFO] Attempting to retrieve quote...')
+    sql_object = get_rand_sql(conn)
+    if sql_object is None:
         await send_embed(ctx, ':x:\tThere are currently no quotes stored!\t:x:', 'Try using !add to add new quotes', discord.Color.red(),'','')
+        print('[ERROR] Unable to retrieve quote')
         return
 
-    rand_quote = random.choice(os.listdir(all_path))
-    full_path = os.path.join(all_path, rand_quote)
-    rand_file = open(full_path, 'r')
-    created_user = rand_file.readline().rstrip('\n')
+    user = sql_object.name
+    content = sql_object.quote
+    date = sql_object.date
 
-    file_name_extension= os.path.basename(full_path)
-    file_name = os.path.splitext(file_name_extension)[0]
+    member = await commands.MemberConverter().convert(ctx, user)
 
-    member = await commands.MemberConverter().convert(ctx, created_user)
-    file_content = rand_file.read()
-
-    time_str = file_name.split(' ')
-    time_obj = datetime.strptime(time_str[0], '%m%d%y')
+    time_obj = datetime.strptime(date, '%Y-%m-%d')
     time_stamp = time_obj.strftime('%B %d, %Y')
 
-    await send_embed(ctx, file_content,'', discord.Color.random(), member.display_name, member.avatar_url, time_stamp)
+    await send_embed(ctx, content,'', discord.Color.random(), member.display_name, member.avatar_url, time_stamp)
+    print(f'[SUCCESS] Successfully retrieved quote - @{member.display_name}: \'{content}\'')
 
-def create_dir(str):
+def get_rand_sql(conn):
+    class SQL(object):
+        pass
+    sql_object = SQL()
+
     try:
-        os.mkdir(str)
-        print('[SUCCESS] Directory', str, 'created!')
-    except FileExistsError:
-        print('[INFO] Directory', str, 'exists - Continuing...')
+        c = conn.cursor()
+        sql = 'SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1;'
+        c.execute(sql)
+        result = c.fetchone()
+        if result is None:
+            return None
+        sql_object.name = result[1]
+        sql_object.quote = result[2]
+        sql_object.date = result[3]
+        return sql_object
+    except sqlite3.Error as error:
+        print('[ERROR] Unable to retrieve random quote -', error)
+        return None
 
 
 async def send_embed(ctx, title, description, color, author, icon, footer=None):
@@ -120,4 +151,4 @@ async def send_embed(ctx, title, description, color, author, icon, footer=None):
     embed.set_author(name=author, icon_url=icon)
     await ctx.send(embed=embed)
 
-client.run(os.getenv('TOKEN'))
+client.run(os.environ.get('TOKEN'))
