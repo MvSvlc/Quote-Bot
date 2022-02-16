0071268f7d8cfd009f28b81e70a842dfa350e08a
Mason Salcido <salcido.mts@gmail.com>
2022-02-15 23:23:10 -0700

    Added ability to change user and quotes remain Allowed specifying user in retrieving quotes Some cleanup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 from datetime import datetime
 import os
+from re import A, T
 import sqlite3
 import discord
 from discord.ext import commands
@@ -43,7 +44,7 @@ database_name = cfg.get('database','Name')
 database = rf'{database_name}.db'
 sql_create_quotes_table = ''' CREATE TABLE IF NOT EXISTS quotes (
     id INTEGER PRIMARY KEY,
-    name text NOT NULL,
+    name int NOT NULL,
     quote text NOT NULL,
     create_date text NOT NULL); '''
 
@@ -65,7 +66,7 @@ async def on_ready():
     print('{0.user} is Online!'.format(client))
 
 @client.command()
-async def add(ctx, user='None' ,*args):
+async def add(ctx, user: discord.User, *args):
 
     if len(args) < 1:
         await send_embed(ctx, ':x:\tUnable to process command\t:x:', '!add @user <quote>', discord.Color.red(), '', '')
@@ -75,68 +76,84 @@ async def add(ctx, user='None' ,*args):
     print('[INFO] Attempting to create new quote...')
     await send_embed(ctx, ':repeat:\tAttempting to create quote...\t:repeat:', '', discord.Color.teal(), '', '')
     await asyncio.sleep(3)
-    
-    removed_chars = '<@!>'
-    for character in removed_chars:
-        user = user.replace(character,'')
 
-    try:
-        user_object = await client.fetch_user(user)
-    except (discord.errors.NotFound, discord.errors.HTTPException):
-        await send_embed(ctx,':x:\tUnable to locate @{}\t:x:'.format(user), 'Must tag user', discord.Color.red(),'','')
+    if user is None:
+        await send_embed(ctx,f':x:\tUnable to locate {user}\t:x:', 'Must tag user', discord.Color.red(),'','')
         print('[ERROR] Unable to located user')
         return
 
-    user_str = str(user_object)
-
-
     content = ' '.join(args)
-    create_insert_sql(conn, user_str, content)
+    create_insert_sql(conn, user.id, content)
 
     await send_embed(ctx, ':white_check_mark:\tSuccessfully created quote\t:white_check_mark:', '"{}"'.format(' '.join(args)), discord.Color.green(),'', '')
-    print(f'[SUCCESS] Inserted new quote to DB - @{user_str}: \'{content}\'')
+    print(f'[SUCCESS] Inserted new quote to DB - @{user.display_name}: \'{content}\'')
 
 @client.command()
-async def quote(ctx):
+async def quote(ctx, user: discord.User = None):
 
     print('[INFO] Attempting to retrieve quote...')
-    sql_object = get_rand_sql(conn)
-    if sql_object is None:
-        await send_embed(ctx, ':x:\tThere are currently no quotes stored!\t:x:', 'Try using !add to add new quotes', discord.Color.red(),'','')
-        print('[ERROR] Unable to retrieve quote')
-        return
 
-    user = sql_object.name
-    content = sql_object.quote
-    date = sql_object.date
+    if user is None:
+        sql_object = get_rand_sql(conn)
+        if sql_object is None:
+            await send_embed(ctx, ':x:\tThere are currently no quotes stored!\t:x:', 'Try using !add to add new quotes', discord.Color.red(),'','')
+            return
+        await send_quote(ctx, sql_object)
+    else:
+        sql_object = get_rand_sql(conn, user.id)
+        if sql_object is None:
+            await send_embed(ctx, f':x:\tThere are currently no quotes stored for {user.display_name}\t:x:', f'Try adding one with !add @{user} <quote>!', discord.Color.red(), '','')
+            return
+        await send_quote(ctx, sql_object)
 
-    member = await commands.MemberConverter().convert(ctx, user)
 
+async def send_quote(ctx, sql_obj):
+    user_id = sql_obj.name
+    content = sql_obj.quote
+    date = sql_obj.date
+
+    member = await client.fetch_user(user_id)
+    
     time_obj = datetime.strptime(date, '%Y-%m-%d')
     time_stamp = time_obj.strftime('%B %d, %Y')
-
     await send_embed(ctx, f'\"{content}\"','', discord.Color.random(), member.display_name, member.avatar_url, time_stamp)
-    print(f'[SUCCESS] Successfully retrieved quote - @{member.display_name}: \'{content}\'')
 
-def get_rand_sql(conn):
+
+def get_rand_sql(conn, user = None):
     class SQL(object):
         pass
     sql_object = SQL()
 
-    try:
-        c = conn.cursor()
-        sql = 'SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1;'
-        c.execute(sql)
-        result = c.fetchone()
-        if result is None:
+    c = conn.cursor()
+
+    if user is None:
+        try:
+            sql = 'SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1;'
+            c.execute(sql)
+            result = c.fetchone()
+            if result is None:
+                return None
+            sql_object.name = result[1]
+            sql_object.quote = result[2]
+            sql_object.date = result[3]
+            return sql_object
+        except sqlite3.Error as error:
+            print('[ERROR] Unable to retrieve random quote -', error)
             return None
-        sql_object.name = result[1]
-        sql_object.quote = result[2]
-        sql_object.date = result[3]
-        return sql_object
-    except sqlite3.Error as error:
-        print('[ERROR] Unable to retrieve random quote -', error)
-        return None
+    else:
+        try:
+            sql = f'SELECT * FROM quotes WHERE name = {user} ORDER BY RANDOM() LIMIT 1;'
+            c.execute(sql)
+            result = c.fetchone()
+            if result is None:
+                return None
+            sql_object.name = result[1]
+            sql_object.quote = result[2]
+            sql_object.date = result[3]
+            return sql_object
+        except sqlite3.Error as error:
+            print(f'[ERROR] Unable to retrieve random quote from {user} -', error)
+            return None
 
 
 async def send_embed(ctx, title, description, color, author, icon, footer=None):
